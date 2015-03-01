22f188173bc11eb70b220f50826070a96b9776f8
Jessica McElroy <mcelroyjessica@gmail.com>
2015-02-28 16:08:32 -0800

    pulling in dataset from socrata api and displaying color-coded markers on map

--- js/map.js
+++ js/map.js
@@ -1,3 +1,5 @@
+var TOOLTIP = _.template($("#tooltip").html());
+
 var initializeMap = function(){
 	L.mapbox.accessToken = 'pk.eyJ1Ijoiam1jZWxyb3kiLCJhIjoiVVg5eHZldyJ9.FFzKtamuKHb_8_b_6fAOFg';
 	var map = L.mapbox.map('map-container', 'jmcelroy.k6hc0kie')
@@ -5,32 +7,104 @@ var initializeMap = function(){
 	return map;
 };
 
-var placeMarkers = function(){
-	var geoJSON = createGeoJson(dummyData);
-	L.mapbox.featureLayer(geoJSON).addTo(window.map);
+var placeMarkers = function(data){
+
+	var geoJSON = createGeoJson(data);
+
+	var markerLayer = L.mapbox.featureLayer();
+
+	// define custom HTML for popups
+	markerLayer.on('layeradd', function(e){
+		var marker = e.layer;
+		var markerProps = marker.feature.properties;
+
+		var popupContent = TOOLTIP({
+			address: markerProps.address,
+			neighborhood: markerProps.neighborhood,
+			description: markerProps.description,
+			zoning: markerProps.zoning,
+			units: markerProps.units,
+			status: markerProps.status
+		});
+
+		marker.bindPopup(popupContent);
+	});
+
+	// Add them all to the map 
+	markerLayer.setGeoJSON(geoJSON);
+	markerLayer.addTo(window.map);
 };
 
+var getData = function(){
+	$.get('https://data.sfgov.org/resource/n5ik-nmm3.json', function(data){
+		placeMarkers(data);
+	});
+};
 
-var createGeoJson = function(data){
+var createGeoJson = function(projects){
 
 	var featureCollection = {
 		type: "FeatureCollection",
 		features: []
 	};
 
-	for (var i = 0; i < data.length; i++) {
+	var markerType;
+
+	for (var i = 0; i < projects.length; i++) {
+
+		switch (projects[i].zoning_generalized) {
+			case "Commercial" || "Neighborhood Commercial":
+				markerType = "commercial";
+				break;
+			case "Public":
+				markerType = "town-hall";
+				break;
+			case "Residential" || "High Density Residential":
+				markerType = "building";
+				break;
+			case "Mixed Use":
+				markerType = "town";
+				break;
+			case "Industrial":
+				markerType = "warehouse";
+				break;
+			default:
+				markerType = "building";
+		}
+
+		var markerColor;
+
+		var status = projects[i].beststat_group.trim();
+
+		if (status === "Construction") {
+			markerColor = "#04ff54";
+		}
+		else if (status.substring(0,2) === "PL" || status.substring(0,2) === "Pl") {
+			markerColor = '#f44';
+		}
+		else if (status.substring(0,2) === "BP") {
+			markerColor = "#307de1";
+		}
+
+		var address = projects[i].location_1.human_address.split(",");
+		address = address[0].substring(11).replace(/["']/g, "");
+
 		featureCollection.features.push({
     		type: "Feature",
 	        geometry: {
 	        	"type": "Point", 
-	        	"coordinates": [data[i].longitude, data[i].latitude]
+	        	"coordinates": [projects[i].location_1.longitude, projects[i].location_1.latitude]
 	        },
 	        properties: {
-		        title: data[i].address,
-		        description: data[i].description,
+		        address: address,
+		        neighborhood: projects[i].planning_neighborhood,
+		        description: projects[i].planning_project_description || projects[i].dbi_project_description,
+		     	zoning: projects[i].zoning_generalized,
+		        units: projects[i].units,
+		        status: projects[i].beststat_group,
 		        'marker-size': 'medium',
-		        'marker-color': '#f44', // color code according to status?
-		        'marker-symbol': 'commercial'
+		        'marker-color': markerColor, // color code according to status?
+		        'marker-symbol': markerType
     		}
 		});
 	}
@@ -38,48 +112,7 @@ var createGeoJson = function(data){
 	return featureCollection;
 };
 
-// e.g. of JSON from Seattle in Progress:
-// {"address": "2201 8TH AVE", "
-// latitude": 47.617259979248, 
-// "id": "3018580", 
-// "longitude": -122.339668273926, 
-// "status": "in_review"}
-
-// TODO: get actual json from api
-var dummyData = [
-	{	
-		description: 'Expansion of SFMOMA to SE with 40,000-sf gallery wing at Natoma Street, acquisition and replacement of Heald College building on Howard Street with 62,000-sf new construction for back-of-house and admin functions, vacation of portion of Hunt Alley',
-		address: '151 Third Street 94103',
-		latitude: 37.785935700000003,
-		longitude: -122.40076120000001
-	},
-	{	
-		description: 'THC/Citizens Housing (MOH) proposal for 100% affordable housing project, 15-story, w/ 18 parking spaces and 3,640-gsf retail at ground level.',
-		address: '1400 Mission St',
-		latitude: 37.775286399999999, 
-		longitude: -122.4159598
-	},
-	{
-		description: 'Convert warehouse building to 9 residential units and 21 off-street parking spaces.',
-		address: '1469 Pacific Av',
-		latitude: 37.795060900000003, 
-		longitude: -122.419425
-	},
-	{	
-		description: 'The proposed project would include the temporary removal of the existing Golden Gateway Tennis and Swim Club facility and the new construction of two mixed use buildings and outdoor health club facilities. The new buildings would include 170 residential units.',
-		address: '8 Washington Street',
-		latitude: 37.795891599999997, 
-		longitude: -122.4005452
-	},
-	{
-		description: 'Construction of a new 4-story, 2-unit residential building.',
-		address: '2626 Larkin St',
-		latitude: 37.802238000000003, 
-		longitude: -122.42105100000001
-	}
-];
-
 $(document).ready(function() {
 	window.map = initializeMap();
-	placeMarkers();
+	getData();
 });
