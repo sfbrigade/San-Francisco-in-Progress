5c44c9709f060d23f288ac8e979ef3d0ec099544
nora humpage <nora.humpage@gmail.com>
2015-04-01 12:48:10 -0700

    added several differnt project layouts

--- sf-in-progress/Homepage/js/custom.js
+++ sf-in-progress/Homepage/js/custom.js
@@ -19,9 +19,18 @@ function resizeLanding() {
   }).resize();
 }
 
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
 
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
+
+// jQuery for page scrolling feature
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
