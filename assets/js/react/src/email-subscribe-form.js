27b256e5e201af4d756dc78b255cd2ccd8fb16c0
Jessica McElroy <mcelroyjessica@gmail.com>
2015-06-07 11:13:19 -0700

    jQuery hack bc close button wasn’t working

--- assets/js/react/src/email-subscribe-form.js
+++ assets/js/react/src/email-subscribe-form.js
@@ -13,10 +13,7 @@ module.exports = React.createClass({
 	, submitForm: function () {
 		var url = this.createActionURL()
 		$.post(url, {email: this.state.email})
-		React.render(
-			<SubmissionSuccess close={this.close} />
-			, document.getElementById('projectProfile-container')
-		)
+		$('#email-form').replaceWith('<h3>Email subscribe successful</h3>')
 	}
 	, close: function() {
     var modal = this.getDOMNode()
@@ -39,26 +36,11 @@ module.exports = React.createClass({
 			<div className="form-group" >
 				<div className='glyphicon glyphicon-remove' style={closeStyle} onClick={this.close}></div><br />
 				<h1> Subscribe to {this.props.address} </h1>
-				<p>You will receive email notifications when this project is on the agenda for a public hearing.</p>
-		    <input onChange={this.saveEmail} type="text" name="email" className="form-control" placeholder="example@mail.com" />
-				<button onClick={this.submitForm} style={marginTop} className="btn-info">Subscribe</button>
-			</div>
-		)
-	}
-})
-
-var SubmissionSuccess = React.createClass({
-	render: function () {
-		var closeStyle = {
-      float: 'right',
-      cursor: 'pointer',
-      fontSize: '1.7em'
-    }
-
-		return (
-			<div className="form-group" >
-				<div className='glyphicon glyphicon-remove' style={closeStyle} onClick={this.props.close}></div><br />
-				<h3>Email subscribe successful</h3>
+				<div id="email-form">
+					<p>You will receive email notifications when this project is on the agenda for a public hearing.</p>
+			    <input onChange={this.saveEmail} type="text" name="email" className="form-control" placeholder="example@mail.com" />
+					<button onClick={this.submitForm} style={marginTop} className="btn-info">Subscribe</button>
+				</div>
 			</div>
 		)
 	}
